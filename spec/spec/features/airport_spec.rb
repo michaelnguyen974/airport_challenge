6e62ebda57575f56b879a66373d133e3f9864a43
Michael Nguyen <michaelnguyen974@gmail.com>
2018-12-27 16:34:26 +0000

    planes can take off

--- spec/spec/features/airport_spec.rb
+++ spec/spec/features/airport_spec.rb
@@ -5,8 +5,15 @@ RSpec.describe Airport do
     it 'should land a plane' do  
       airport = Airport.new
       plane = Plane.new
-      expect {airport.land(plane)}.not_to raise_error
+      expect { airport.land(plane) }.not_to raise_error
+    end 
+
+    it 'should have plane to take off' do 
+      airport = Airport.new
+      plane = Plane.new
+      expect{ airport.take_off(plane) }.not_to raise_error
     end 
   end 
+
 
 end 
--- spec/spec/units/airport_unit_spec.rb
+++ spec/spec/units/airport_unit_spec.rb
@@ -3,9 +3,14 @@ require 'airport.rb'
 RSpec.describe Airport do 
   context '#Airport' do 
 
-    it 'looks for one argument' do 
+    it 'expects airport to respond to land' do 
       airport = Airport.new
       expect(airport).to respond_to(:land).with(1).argument
+    end 
+
+    it 'expects airport to respond to take off' do 
+      airport = Airport.new
+      expect(airport).to respond_to(:take_off).with(1).argument
     end 
   end 
 end 
